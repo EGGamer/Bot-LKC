4961319f4659ed503500435c1f402ca7ce9aaebd
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-09 09:55:55 +0200

    Mejorado rich embed de inoperativo

--- index.js
+++ index.js
@@ -15,37 +15,37 @@ bot.on("message", function(message){
         //Si el que ha mandado un mensaje es el bot, no hace nada.
       if (message.author.equals(bot.user)) return;
      //COMANDOS
-        //Si el mensaje no empieza por el prefix, no hace nada.
+        
       if (!message.content.startsWith(PREFIX)) return;
 
       var args = message.content.substring(PREFIX.length).split(" ");
-        //Aqui pasa todo a minuscula
+        
       switch (args[0].toLowerCase())
-      { //Si pone "-k ping" responde "Pong!""
+      { 
         case "ping":
              message.channel.send("Pong!");
-             break;
-        //Si pone "-k info" salta un mensaje con info del bot.       
+             break;               
         case "info":
         message.channel.send("!Soy el Kraken! ¡Soy el guardián de este servidor! He sido creado por **EG Gamer** y **Aeirety**")
-        break;
-        //Si pide ayuda se la da.
+        message.delete();
+        break;       
         case "ayuda":
        message.channel.send("¿Necesitas mi ayuda? Soy un kraken, no si si voy a ser de mucha ayuda, pero aquí tienes los comandos disponibles: ```-k info | -k ayuda | -k ping```");
-       break;
-       //En caso de que el bot esté inoperativo
+       message.delete();
+       break;       
        case "botinoperativo":
        {
+         
            var embed = new Discord.RichEmbed()
            embed.setColor(0xf85959);     
            embed.setTitle("BOT INOPERATIVO");
            embed.setDescription("El bot no se podrá usar hasta nuevo aviso. @everyone");
-           
+           embed.setThumbnail("https://cdn.discordapp.com/avatars/454682928769663007/14ac96f716c195bf55d7373778bd092c.png");
            embed.setTimestamp();
            message.channel.send(embed);
+           message.delete();
        }
        break;
-       //Si no es ninguno de los anteriores comandos pone Comando Invalido.
         default:
         message.channel.send("```Comando no válido```");
       }
