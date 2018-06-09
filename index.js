53d68c1296abdc69a0b29bd9ccaa7c05262d6045
EGGamer <40073654+EGGamer@users.noreply.github.com>
2018-06-09 11:50:20 +0200

    editado rpesentacion

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on("message", function(message){
       switch (args[0].toLowerCase())
       { 
         case "presentacion":
-        message.channel.send("¡HOLA @everyone ! Soy el kraken, el actual bot supremo de este servidor. Me podréis utilizar (casi) siempre que queráis. A medida que pase el tiempo tendré más utilidades. He sido creado gracias a EG Gamer y Aeirety. Un saludo grumetes. Y no os portéis mal, ¡que os llevo a las profunfidades del mar!")
+        message.channel.send("¡HOLA @everyone ! Soy el kraken, el actual bot supremo de este servidor. Me podréis utilizar (casi) siempre que queráis. A medida que pase el tiempo tendré más utilidades. He sido creado por EG Gamer con ayuda de Aeirety. Un saludo grumetes. Y no os portéis mal, ¡que os llevo a las profunfidades del mar!")
         message.delete();
         break;
 
